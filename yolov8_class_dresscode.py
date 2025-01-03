a91555e94f621abb15cb25eae5baed8ff2545942
yasmine1log <yassmine.loghmari4@gmail.com>
2025-01-03 02:09:54 +0100

    Initial commit for Dress Code YOLOv8 project

--- yolov8_class_dresscode.py
+++ yolov8_class_dresscode.py
@@ -8,5 +8,3 @@ model = YOLO("yolov8n.pt")
 # Train the model on the COCO8 example dataset for 100 epochs
 results = model.train(data="data.yaml", epochs=100, imgsz=640)
 
-# Run inference with the YOLOv8n model on the 'bus.jpg' image
-results = model("path/to/bus.jpg")
